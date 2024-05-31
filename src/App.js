de117fd62b6c340567e67d05d171e5c870680e36
deysiSc76 <deysi.santacruz76@gmail.com>
2024-05-31 18:16:00 -0400

    Api-pokemon con axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=807');
+      setPokemons(response.data.results.map(p => p.name));
+    } catch (error) {
+      console.error('Hubo un error al obtener los datos de los Pokémon:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Llamamos a fetchPokemons aquí para cargar los datos al montar el componente
+    fetchPokemons();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => fetchPokemons()}>Fetch Pokemon</button>
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>{pokemon}</li>
+        ))}
+      </ul>
     </div>
   );
 }
